3c52cc38f385e6e736a9adc4c70e0012850cc1be
bihoqo <adamshagash@gmail.com>
2020-11-25 07:57:32 +0200

    fix App.js

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import { View, StyleSheet, StatusBar } from 'react-native';
+import { View } from 'react-native';
 import HomePage from './screens/HomePage.js';
 
 export default function App() {
   return (
     <View style={{flex: 1}}>
-      <View>
-        <StatusBar barStyle = "dark-content" hidden = {false} backgroundColor = "gray" translucent = {true}/>
-      </View>
       <HomePage></HomePage>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  appHeader: {
-    backgroundColor: 'black',
-    paddingTop: StatusBar.currentHeight
-  }
-});
